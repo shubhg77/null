86af1f84515d7ffc65d2d9b489aa2a9aa53f95a4
shubh sharma <109013021+shubhg77@users.noreply.github.com>
2023-03-19 17:48:39 +0530

    find data types limits

---
+++ find limits of datatypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+cout <<"\n check uppper and limits of integer :\n";
+cout <<"the max limit of int data type :"<< INT_MAX<<endl;
+cout <<"the min limit of int data type:"<< INT_MIN <<endl;
+cout <<"the max limit of float data type :"<< FLT_MAX <<endl;
+cout << "the max limit of char data type ;"<< CHAR_MAX <<endl;
+	return 0;
+}
